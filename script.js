413201f194f92b96a7806db3633229fd305f3d1d
menachemlev <86170841+menachemlev@users.noreply.github.com>
2021-07-26 20:43:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 "use strict";
-//Determine which videos to play
-function playVideos(entries, observer) {
-  console.log(entries);
+ /*The app is going through all the entries(YouTube iframes) every time the page is scrolling.
+When a video is at-least 60% visible it will start playing and rest of the videos will stop. To change to playing state,
+the page is modifying the source attribute ,changing the parameteres 'autoplay' and 'mute'(must come together)*/
+function playVideos(entries, observer) { 
   entries.forEach((entry) => {
     const SRC = entry.target.getAttribute("src");
     //If it's intersecting play video
@@ -9,14 +10,17 @@ function playVideos(entries, observer) {
       entry.isIntersecting ? "autoplay=1&mute=1" : "autoplay=0&mute=0"
     }`;
     entry.target.setAttribute("src", updateSRC);
-    console.log(entry.target.getAttribute("src"));
   });
 }
 
-//Observer setup
+
+
+
+//Setting up the observer observe and to play each video when it is at-least 60% percent visible.
 const observer = new IntersectionObserver(playVideos, {
   root: null,
   threshold: 0.6,
 });
 
+//Every iframe is observed
 document.querySelectorAll("iframe").forEach((vid) => observer.observe(vid));
